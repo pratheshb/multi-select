c9d5554f6f0c8b4f2587d7841686f8256bb4b796
Prathesh Baskaran <Prathesh.Baskaran@genesys.com>
2022-12-01 20:31:21 +0530

    combine simple components

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,14 +1,18 @@
-import './Options.css'
-import Option from '../Option/Option'
+import './Options.css';
 export default function Options(props) {
     return (
         <div className="drop-down-container">
-        {
-            props.options.filter(option => !props.SelectedOptions.includes(option)).map(
-                (option, index) => (
-                <Option key={index} option={option} onSelect={props.onSelect} />
-                ))
-        }
+            {
+                props.options.filter(option => !props.SelectedOptions.includes(option)).map(
+                    (option, index) => (
+                        <div
+                            key={index}
+                            onClick={() => props.onSelect(option)}
+                            className='option'>
+                            {option}
+                        </div>
+                    ))
+            }
         </div>
     )
 }
